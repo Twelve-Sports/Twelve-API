e97fea2ab2be59d69c8d5c29e69893fd35aea9d5
Pedro Lobato <Pedrosslobato@gmail.com>
2023-08-28 11:01:20 -0300

    :card_file_box: added conection datase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { userModule } from './user/user.module';
+import { UserEntity } from './user/user.entity';
+import { ClientEntity } from './client/client.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'lobatoSQL',
+      password: '1234',
+      database: 'PI',
+      entities: [UserEntity, ClientEntity],
+      synchronize: true,
+    }),
+    userModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
