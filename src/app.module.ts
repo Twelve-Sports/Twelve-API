6776bc77d2f1f3632c7e8d1f478194ec14fda20f
Pedro Lobato <Pedrosslobato@gmail.com>
2023-08-30 10:09:13 -0300

    :hammer: added all tables in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { userModule } from './user/user.module';
 import { UserEntity } from './user/user.entity';
 import { ClientEntity } from './client/client.entity';
+import { AdminEntity } from './admin/admin.entity';
+import { adminModule } from './admin/admin.module';
+import { reserveModule } from './reserve/reverve.module';
+import { ReserveEntity } from './reserve/reserve.entity';
+import { sportModule } from './sport/sport.module';
+import { SportEntity } from './sport/sport.entity';
+import { CourtEntity } from './court/court.entity';
+import { courtModule } from './court/court.module';
 
 @Module({
   imports: [
@@ -15,10 +23,21 @@ import { ClientEntity } from './client/client.entity';
       username: 'lobatoSQL',
       password: '1234',
       database: 'PI',
-      entities: [UserEntity, ClientEntity],
+      entities: [
+        UserEntity,
+        ClientEntity,
+        AdminEntity,
+        ReserveEntity,
+        SportEntity,
+        CourtEntity,
+      ],
       synchronize: true,
     }),
     userModule,
+    adminModule,
+    reserveModule,
+    sportModule,
+    courtModule,
   ],
   controllers: [AppController],
   providers: [AppService],
