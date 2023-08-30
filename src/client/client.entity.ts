be11a715a68e823db81908f7699f83e8a25cdde5
Pedro Lobato <Pedrosslobato@gmail.com>
2023-08-30 10:14:12 -0300

    :truck: changing table name

--- src/client/client.entity.ts
+++ src/client/client.entity.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from 'src/user/user.entity';
 import { Column, Entity } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'client' })
 export class ClientEntity extends UserEntity {
   @Column()
   isVerified: boolean;
