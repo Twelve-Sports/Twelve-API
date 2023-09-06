50c42aad3b0b0586e38026c5da53cedb2e7fa435
Pedro Lobato <Pedrosslobato@gmail.com>
2023-09-06 19:31:03 -0300

    🔄 added one-to-many relationship with Reserve table

--- src/client/client.entity.ts
+++ src/client/client.entity.ts
@@ -1,5 +1,6 @@
+import { ReserveEntity } from 'src/reserve/reserve.entity';
 import { UserEntity } from 'src/user/user.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity({ name: 'client' })
 export class ClientEntity extends UserEntity {
@@ -8,4 +9,7 @@ export class ClientEntity extends UserEntity {
 
   @Column()
   isSocial: boolean;
+
+  @OneToMany(() => ReserveEntity, (reserve) => reserve.client)
+  reserves: ReserveEntity[];
 }
