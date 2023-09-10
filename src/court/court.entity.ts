4356243ae2fe6a0826c9dbf3d7e3aa7d86270106
Pedro Lobato <pedrosslobato@gmail.com>
2023-09-10 11:45:38 -0300

    :hammer: creado relacionamento muitos para muitos com esports

--- src/court/court.entity.ts
+++ src/court/court.entity.ts
@@ -1,7 +1,21 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { SportEntity } from 'src/sport/sport.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity({ name: 'court' })
 export class CourtEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => SportEntity)
+  @JoinTable()
+  sports: SportEntity[];
 }
