7a7ba7c9d7671337b01961de93cef669047306ed
pedro <pedrosslobato@gmail.com>
2023-09-20 18:01:16 -0300

    🔧 ┇ Validaçoes para criação de cliente

--- src/client/dto/client-create.dto.ts
+++ src/client/dto/client-create.dto.ts
@@ -1,15 +1,24 @@
-import { IsString,IsEmail } from "class-validator";
+import { IsString,IsEmail, Length, Matches, IsBoolean } from "class-validator";
 
 export class clientCreateDTO{
     @IsString()
+    @Length(2, 100) 
     name: string;
 
     @IsEmail()
     email: string;
 
     @IsString()
+    @Length(8, 100) 
     password: string;
 
     @IsString()
+    @Length(8, 15) 
     phone: string;
+
+    @IsBoolean()
+    isVerified: boolean;
+
+    @IsBoolean()
+    isSocial: boolean;
 }
