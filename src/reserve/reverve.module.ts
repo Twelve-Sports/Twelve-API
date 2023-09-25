cebdfc54f3df2590a16b283b89cbac0c5702dd21
Juan Brys <juanbrys@gmai.com>
2023-09-25 11:39:26 -0300

    🔧 ┇ Atualizado dependencia do modulo de reserva

--- src/reserve/reverve.module.ts
+++ src/reserve/reverve.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReserveEntity } from './reserve.entity';
+import { ReserveController } from './reserve.controller';
+import { ReserveService } from './reserve.service';
+import { ClientEntity } from 'src/client/client.entity';
+import { CourtEntity } from 'src/court/court.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReserveEntity])],
-  controllers: [],
+  imports: [TypeOrmModule.forFeature([ReserveEntity, ClientEntity, CourtEntity])],
+  controllers: [ReserveController],
+  providers: [ReserveService ],
 })
-export class reserveModule {}
+export class reserveModule {
+
+}
