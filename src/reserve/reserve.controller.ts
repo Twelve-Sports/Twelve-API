f9a50c4486531ac399346afa94b47bd474f49e85
Juan Brys <juanbrys@gmai.com>
2023-09-25 11:35:41 -0300

    🔧 ┇Criada a rota get e post

--- src/reserve/reserve.controller.ts
+++ src/reserve/reserve.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post, Body } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ReserveEntity } from "./reserve.entity";
+import { Repository } from "typeorm";
+import { ReserveService } from "./reserve.service";
+import { CreateReserveDto } from "./dto/reserve-create.dto";
 
-@Controller()
-export class ReserveController {}
+
+@Controller('reserve')
+export class ReserveController {
+ constructor(
+    @InjectRepository(ReserveEntity)
+    private reserveRepository: Repository<ReserveEntity>,
+    private reserveService: ReserveService
+ ) {}
+
+ @Get()
+    async findAll(): Promise<ReserveEntity[]> {
+        return await this.reserveService.findAll();
+    }
+
+@Post()
+    async createReserve(@Body() createReserveDto: CreateReserveDto) {
+      return await this.reserveService.createReserve(createReserveDto);
+    }
+}
